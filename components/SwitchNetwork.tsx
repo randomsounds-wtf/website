e49248e779633eada4f2a8a4fa2f38387a9a510d
v1rtl <pilll.pl22@gmail.com>
2022-04-23 20:02:41 +0300

    reload page after switching connection

--- components/SwitchNetwork.tsx
+++ components/SwitchNetwork.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { ethers } from 'ethers'
-import { WalletProps } from '../lib/types'
 
 export const SwitchNetwork = ({ provider }: { provider: ethers.providers.Web3Provider }) => (
   <button
@@ -9,15 +8,17 @@ export const SwitchNetwork = ({ provider }: { provider: ethers.providers.Web3Pro
       cursor: 'pointer'
     }}
     onClick={() =>
-      provider.send('wallet_addEthereumChain', [
-        {
-          chainId: ethers.utils.hexlify(process.env.NEXT_PUBLIC_NETWORK_ID!),
-          chainName: 'Polygon network',
-          nativeCurrency: { name: 'MATIC', symbol: 'MATIC', decimals: 18 },
-          rpcUrls: ['https://rpc-mainnet.maticvigil.com'],
-          blockExplorerUrls: ['https://polygonscan.com/']
-        }
-      ])
+      provider
+        .send('wallet_addEthereumChain', [
+          {
+            chainId: ethers.utils.hexlify(process.env.NEXT_PUBLIC_NETWORK_ID!),
+            chainName: 'Polygon network',
+            nativeCurrency: { name: 'MATIC', symbol: 'MATIC', decimals: 18 },
+            rpcUrls: ['https://rpc-mainnet.maticvigil.com'],
+            blockExplorerUrls: ['https://polygonscan.com/']
+          }
+        ])
+        .then(() => location.reload())
     }
   >
     Switch to Polygon
